c31c86e3b26e917ffe073820e7ca3e2c25a55380
Валентин Лопатин <162607283+fotiloli@users.noreply.github.com>
2024-06-01 20:34:15 +0300

    Delete solutions/10-recursion.js

--- solutions/10-recursion.js
+++
@@ -1,9 +0,0 @@
-const sequenceSum = (begin, end) => {
-  // BEGIN
-  if(begin>end) return NaN;
-  if(begin===end) return begin;
-  return begin + sequenceSum(begin + 1, end);
-  // END
-};
-
-export default sequenceSum;
